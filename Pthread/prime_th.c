9c8a39e2cb433ec80e05616ed3da3bba6c7f183c
nanoha25 <nanohachina@takamachi.onmicrosoft.com>
2016-06-08 19:25:49 +1000

    Complete algorithm
    And add a lot of debugging output, to make sure every single number can
    be normally processed, whether prime or not.

--- Pthread/prime_th.c
+++ Pthread/prime_th.c
@@ -31,25 +31,34 @@ int *find_prime(void *threadarg)
     {
       if (i%2==0)
       {
+        printf("%d Not a prime (2)",i);
         break;
       }
       else if (i%3==0)
       {
+        printf("%d Not a prime (3)",i);
         break;
       }
       else if (i%5==0)
       {
+        printf("%d Not a prime (5)",i);
         break;
       }
       else if (i%7==0)
       {
+        printf("%d Not a prime (7)",i);
         break;
       }
       else
       {
-        count_mtx = count_mtx + 1;
+        //program runs to here, there should be a found prime.
+        printf("Found a prime: %d",i);
+        mycount = mycount + 1;
       }
     }
+    pthread_mutex_lock (&count_mtx);
+    count = count + mycount + 4; //Here adds 4 because there are already 4 prime numbers. This "4" can also be added at the end of program.
+    pthread_mutex_unlock (&count_mtx);
   }
   else
   {
@@ -57,24 +66,41 @@ int *find_prime(void *threadarg)
     {
       if (i%2==0)
       {
+        printf("%d Not a prime (2)",i);
         break;
       }
       else if (i%3==0)
       {
+        printf("%d Not a prime (3)",i);
         break;
       }
       else if (i%5==0)
       {
+        printf("%d Not a prime (5)",i);
         break;
       }
       else if (i%7==0)
       {
+        printf("%d Not a prime (7)",i);
         break;
       }
       else
       {
-        count_mtx = count_mtx + 1;
+        printf("Found a prime: %d",i);
+        mycount = mycount + 1;
       }
     }
+    /*
+    Here, thread locks mutex, update global prime count, then unlock
+    */
+    pthread_mutex_lock (&count_mtx);
+    count = count + mycount;
+    pthread_mutex_unlock (&count_mtx);
+
+    /*Quit thread. */
+    pthread_exit(NULL);
   }
+
+
+
 }
